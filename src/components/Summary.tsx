e1db4943ce75d1324ade82a5a87404ca0a4fc38a
Gümrah Sindar <gsindar@gmail.com>
2023-06-14 21:08:21 +0300

    add summary section

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -1,15 +1,45 @@
+// styles
+import { useState } from 'react'
+import styles from './Summary.module.scss'
+
+import data from '../../data/db.json'
+
+type Item = {
+  icon: string
+  category: string
+  score: number
+  color: string
+}
+
 const Summary = () => {
+  // fetch data from API using useEffect
+  const [items] = useState<Item[]>(data)
+
   return (
-    <div>
+    <section className={styles.summary}>
       <h3>Summary</h3>
-      <ul>
-        <li>Reaction</li>
-        <li>Memory</li>
-        <li>Verbal</li>
-        <li>Visual</li>
-      </ul>
+      {items &&
+        items.map((item, index: number) => (
+          <ul key={index}>
+            <li
+              style={{
+                color: item.color,
+                backgroundColor: `${item.color}10`,
+              }}
+            >
+              <h4>
+                <img src={item.icon} />
+                <span>{item.category}</span>
+              </h4>
+              <p>
+                {item.score}
+                <span> / 100</span>
+              </p>
+            </li>
+          </ul>
+        ))}
       <button>Continue</button>
-    </div>
+    </section>
   )
 }
 export default Summary
